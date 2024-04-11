593b8ced0a98c37aa234f8420915a7e5eaecf245
Manuel Francos <manuelfrancosro@gmail.com>
2024-04-11 18:29:34 +0100

    Primer comment

---
+++ proyecto/src/test/kotlin/com/manuel/proyecto/ProyectoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.manuel.proyecto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
